8c0d4ee999d92b8d461b876983782555a3d3d8c8
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2024-04-24 21:05:31 -0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,13 @@
+def myfunc(val1,val2):
+    return val1+val2
+
+while True:
+    val1 = int(input("Valor 1: "))
+    val2 = int(input("Valor 2: "))
+
+
+    res = myfunc(val1, val2)
+    print(val1, "+", val2, "=", res)
+
+
+#usa o terminal com: python nomedoarquivo.py
